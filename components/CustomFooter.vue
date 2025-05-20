566d2fb08f8a2c246a45fb731bc380bf7858a73a
Tomasz Slapinski <tom.slapinski@gmail.com>
2025-05-21 00:08:16 +0200

    + dev

---
+++ components/CustomFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="bg-slate-950 text-white py-1 text-center flex justify-between px-2">
+    <div class="flex items-center gap-4 text-sm text-gray-200">
+      Voting App
+    </div>
+    <div>
+      TS & MR
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
