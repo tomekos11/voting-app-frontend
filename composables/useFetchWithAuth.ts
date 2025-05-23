23fe33ed4ecee399393b2936e6ebdb2bd4337999
Tomasz Slapinski <tom.slapinski@gmail.com>
2025-05-23 23:36:16 +0200

    + config -> security + env.example + composable for fetching with auth as client and as ssr

---
+++ composables/useFetchWithAuth.ts
@@ -0,0 +1,54 @@
+export const useFetchWithAuth = async <T>(url: string, options: any = {}): Promise<T> => {
+  const config = useRuntimeConfig();
+
+  let headers = {};
+
+  if (import.meta.server) {
+    const cookie = useRequestHeaders(['cookie']).cookie || '';
+    headers = { cookie };
+  }
+  //   else {
+  //     // Klient — dynamiczny import i odczyt cookies
+  //     const Cookies = (await import('js-cookie')).default;
+  //     const xsrfToken = Cookies.get('XSRF-TOKEN');
+  //     headers = {
+  //       'X-XSRF-TOKEN': xsrfToken || '',
+  //     };
+  //   }
+
+    
+  return $fetch<T>(config.public.API_URL + url, {
+    credentials: 'include',
+    headers,
+    ...options,
+  }).catch(async (error) => {
+    const isSSR = import.meta.server;
+
+    // Logika dla SSR
+    if (isSSR) {
+      const statusCode = error.statusCode || error.response?.status;
+      const isConnectionError = error.code === 'ECONNREFUSED' || error?.cause?.message === 'fetch failed';
+
+      if (statusCode === 500 || isConnectionError) {
+        throw createError({
+          statusCode: 500,
+          statusMessage: 'Błąd połączenia z API',
+          fatal: true,
+        });
+      }
+    }
+    else {
+      const shouldRedirect = 
+        error?.response?.status === 500 ||
+        error.message?.includes('Failed to fetch') ||
+        error.message?.includes('ERR_CONNECTION_REFUSED');
+
+      if (shouldRedirect) {
+        await navigateTo('/500', { redirectCode: 302, replace: true });
+        throw new Error('Client redirect triggered');
+      }
+    }
+
+    throw error;
+  });
+};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,45 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/icon'],
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/icon', 'nuxt-security'],
 
   css: ['~/assets/css/main.css'],
-})
+
+  eslint: {
+    // checker: true,
+  },
+  
+  security: {
+    // rateLimiter: false,
+    nonce: true,
+    headers: {
+      contentSecurityPolicy: {
+        'default-src': ['\'self\''],
+        'connect-src': ['\'self\'', import.meta.env.NUXT_PUBLIC_API_URL, import.meta.env.NUXT_PUBLIC_API_URL_HTTPS],
+        'script-src': ['\'self\'', '\'nonce-{{nonce}}\'', '\'strict-dynamic\''],
+        'style-src': ['\'self\'', '\'nonce-{{nonce}}\''],
+        'img-src': ['\'self\'', 'data:', 'blob:', import.meta.env.NUXT_PUBLIC_API_URL, import.meta.env.NUXT_PUBLIC_API_URL_HTTPS],
+        'font-src': ['\'self\''],
+        'object-src': ['\'none\''],
+        'frame-ancestors' : ['\'none\''],
+        'upgrade-insecure-requests': true,
+      },
+    },
+  },
+
+  routeRules: {
+    '/**': {
+      headers: {
+        'X-Content-Type-Options': 'nosniff',
+        'X-Frame-Options': 'DENY'
+      },
+    }
+  },
+
+  runtimeConfig: {
+    public: {
+      API_URL: import.meta.env.NUXT_PUBLIC_API_URL,
+      API_URL_HTTPS: import.meta.env.NUXT_PUBLIC_API_URL_HTTPS,
+    }
+  }
+});
